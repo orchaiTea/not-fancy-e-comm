3fe4bee349a5028dcf2737151af31f82dada8c36
Pushkar Tripathi <PushkarTripathi@proton.me>
2024-01-07 10:39:07 +0530

    women's section done.

--- src/components/wSection/WSection.jsx
+++ src/components/wSection/WSection.jsx
@@ -1,28 +1,44 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 export default function WSection() {
   const data = useLoaderData();
+  const [currentIndex, SetCurrentIndex] = useState(0);
+
+  const updateIndex = () => {
+    data[currentIndex];
+  };
+
+  const onNextClick = () => {
+    SetCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+  };
+
+  useEffect(() => {
+    updateIndex();
+  }, [currentIndex, data]);
+
   return (
     <div>
       <section className="overflow-hidden">
         <div className="mx-auto max-w-5xl px-5 py-24">
-          {data.map((product) => (
+          {data[currentIndex] ? (
             <div
-              key={product.id}
+              key={data[currentIndex].id}
               className="mx-auto flex flex-wrap items-center lg:w-4/5 pb-8"
             >
               <img
                 alt=""
                 className="h-64 w-full rounded-lg object-cover lg:h-96 lg:w-1/2 object-scale-down"
-                src={product.image}
+                src={data[currentIndex].image}
               />
               <div className="mt-6 w-full lg:mt-0 lg:w-1/2 lg:pl-10">
                 <h1 className="my-4 text-3xl font-semibold text-black">
-                  {product.title}
+                  {data[currentIndex].title}
                 </h1>
-                <p className="leading-relaxed">{product.description}</p>
+                <p className="leading-relaxed">
+                  {data[currentIndex].description}
+                </p>
                 <div className="mb-5 mt-6 flex justify-between items-center border-b-2 border-gray-100 pb-5">
                   <div className="group flex h-11 flex-shrink-0 items-center justify-between overflow-hidden rounded-md border border-gray-300 md:h-12">
                     <button className="text-heading hover:bg-heading flex h-full w-10 flex-shrink-0 items-center justify-center border-e border-gray-300 transition duration-300 ease-in-out focus:outline-none md:w-12">
@@ -36,7 +52,7 @@ export default function WSection() {
                     </button>
                   </div>
                   <div className="title-font text-xl font-bold text-gray-900">
-                    ${product.price}
+                    ${data[currentIndex].price}
                   </div>
                 </div>
                 <div className="flex items-center justify-between">
@@ -46,10 +62,19 @@ export default function WSection() {
                   >
                     Add to Cart
                   </button>
+                  <button
+                    type="button"
+                    className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    onClick={onNextClick}
+                  >
+                    Next
+                  </button>
                 </div>
               </div>
             </div>
-          ))}
+          ) : (
+            <h1>Oops! Something went wrong.</h1>
+          )}
         </div>
       </section>
     </div>
@@ -61,7 +86,6 @@ export const WSectionInfoLoader = async () => {
     const res = await axios.get(
       "https://fakestoreapi.com/products/category/women's clothing"
     );
-    console.log(res.data);
     return res.data;
   } catch (error) {
     console.log(error);
