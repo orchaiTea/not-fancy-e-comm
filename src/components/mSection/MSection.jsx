ed09d1d1c7d63c507631c3a5ded83c547068c397
Pushkar Tripathi <PushkarTripathi@proton.me>
2024-01-11 10:41:01 +0530

    increment,decrement implemented.

--- src/components/mSection/MSection.jsx
+++ src/components/mSection/MSection.jsx
@@ -5,6 +5,7 @@ import { useLoaderData } from "react-router-dom";
 export default function MSection() {
   const data = useLoaderData();
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [count, setCount] = useState(1);
 
   const updateIndex = () => {
     data[currentIndex];
@@ -42,13 +43,19 @@ export default function MSection() {
                 </p>
                 <div className="mb-5 mt-6 flex justify-between items-center border-b-2 border-gray-100 pb-5">
                   <div className="group flex h-11 flex-shrink-0 items-center justify-between overflow-hidden rounded-md border border-gray-300 md:h-12">
-                    <button className="text-heading hover:bg-heading flex h-full w-10 flex-shrink-0 items-center justify-center border-e border-gray-300 transition duration-300 ease-in-out focus:outline-none md:w-12">
+                    <button
+                      onClick={() => setCount(count + 1)}
+                      className="text-heading hover:bg-heading flex h-full w-10 flex-shrink-0 items-center justify-center border-e border-gray-300 transition duration-300 ease-in-out focus:outline-none md:w-12"
+                    >
                       +
                     </button>
                     <span className="duration-250 text-heading flex h-full w-12  flex-shrink-0 cursor-default items-center justify-center text-base font-semibold transition-colors ease-in-out  md:w-20 xl:w-24">
-                      1
+                      {count}
                     </span>
-                    <button className="text-heading hover:bg-heading flex h-full w-10 flex-shrink-0 items-center justify-center border-s border-gray-300 transition duration-300 ease-in-out focus:outline-none md:w-12">
+                    <button
+                      onClick={() => count > 0 && setCount(count - 1)}
+                      className="text-heading hover:bg-heading flex h-full w-10 flex-shrink-0 items-center justify-center border-s border-gray-300 transition duration-300 ease-in-out focus:outline-none md:w-12"
+                    >
                       -
                     </button>
                   </div>
@@ -74,7 +81,7 @@ export default function MSection() {
               </div>
             </div>
           ) : (
-            <h1>Sorry!</h1>
+            <h1>Sorry! Looks like Something went wrong.</h1>
           )}
         </div>
       </section>
