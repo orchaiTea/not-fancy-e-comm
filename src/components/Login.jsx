2a6ac8bb370cccf92b9b2d4ab9c9fe8ebca475fe
Pushkar Tripathi <PushkarTripathi@proton.me>
2024-02-19 06:45:23 +0530

    auth layout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,11 +8,7 @@ function Login() {
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
-        <div className="mb-2 flex justify-center">
-          <span className="inline-block w-full max-w-[100px]">
-            {/* <Logo width="100%" /> */}
-          </span>
-        </div>
+        <div className="mb-2 flex justify-center"></div>
         <h2 className="text-center text-2xl font-bold leading-tight">
           Sign in to your account
         </h2>
@@ -25,7 +21,6 @@ function Login() {
             Sign Up
           </Link>
         </p>
-        {/* {error && <p className="text-red-600 mt-8 text-center">{error}</p>} */}
 
         <form onSubmit={""} className="mt-8">
           <div className="space-y-5">
@@ -33,22 +28,11 @@ function Login() {
               label="Email: "
               placeholder="Enter your email"
               type="email"
-              // {...register("email", {
-              //   required: true,
-              //   validate: {
-              //     matchPatern: (value) =>
-              //       /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-              //       "Email address must be a valid address",
-              //   },
-              // })}
             />
             <Input
               label="Password: "
               type="password"
               placeholder="Enter your password"
-              // {...register("password", {
-              //   required: true,
-              // })}
             />
             <Button
               type="submit"
