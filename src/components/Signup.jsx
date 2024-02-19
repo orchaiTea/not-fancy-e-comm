dcb1fe84cfe852d47b0a5b7d67f0c830bf7b5aaa
Pushkar Tripathi <pushkar.moonscape794@passfwd.com>
2024-02-19 07:42:39 +0530

    a word of caution.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -9,6 +9,9 @@ function Signup() {
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
         <div className="mb-2 flex justify-center"></div>
+        <p className="text-center text-red-600">
+          "Authentication functionality is currently under construction."
+        </p>
         <h2 className="text-center text-2xl font-bold leading-tight">
           Sign up to create account
         </h2>
